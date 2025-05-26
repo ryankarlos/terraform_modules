52a1c8c654fe8b2854aac9d8552d905bbf68b7ef
Ryan Nazareth <ryankarlos@gmail.com>
2025-05-26 22:56:03 +0100

    lb move from internal to public

--- aws/load_balancer/application/main.tf
+++ aws/load_balancer/application/main.tf
@@ -1,7 +1,7 @@
 
-resource "aws_lb" "internal" {
+resource "aws_lb" "lb" {
   name               = var.alb_name
-  internal           = true
+  internal           = false
   load_balancer_type = "application"
   security_groups    = [var.security_group_id]
   idle_timeout = var.idle_timeout
@@ -12,8 +12,8 @@ resource "aws_lb" "internal" {
 }
 
 
-resource "aws_lb_listener" "internal_https" {
-  load_balancer_arn = aws_lb.internal.arn
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.lb.arn
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
@@ -33,7 +33,7 @@ resource "aws_lb_listener" "internal_https" {
 
 
 resource "aws_lb_listener" "frontend_http" {
-  load_balancer_arn = aws_lb.internal.arn
+  load_balancer_arn = aws_lb.lb.arn
   port              = 80
   protocol          = "HTTP"
   default_action {
