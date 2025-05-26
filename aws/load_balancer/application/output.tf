a27ea2106657fb60ca5fc564d51fa6647a1fa31d
Ryan Nazareth <ryankarlos@gmail.com>
2025-05-26 23:40:22 +0100

    update outputs

--- aws/load_balancer/application/output.tf
+++ aws/load_balancer/application/output.tf
@@ -1,16 +1,16 @@
 output "arn" {
   description = "ARN of the internal ALB"
-  value       = aws_lb.internal.arn
+  value       = aws_lb.lb.arn
 }
 
 output "dns" {
   description = "DNS name of the internal ALB"
-  value       = "dualstack.${aws_lb.internal.dns_name}"
+  value       = "dualstack.${aws_lb.lb.dns_name}"
 }
 
 output "zone_id" {
   description = "Canonical hosted zone ID of the load balancer (to be used in a Route 53 Alias record)."
-  value       = aws_lb.internal.zone_id
+  value       = aws_lb.lb.zone_id
 }
 
 
